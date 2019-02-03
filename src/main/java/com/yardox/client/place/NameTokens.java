19a478e7313afdbd08481af2e9d8a53d9985a62d
NSergeyI <yardox@gmail.com>
2019-02-03 14:33:56 +0300

    init comit

---
+++ src/main/java/com/yardox/client/place/NameTokens.java
@@ -0,0 +1,9 @@
+package com.yardox.client.place;
+
+public class NameTokens {
+    public static final String HOME = "/";
+
+    public static String getHOME() {
+        return HOME;
+    }
+}
---
+++ src/main/java/com/yardox/server/guice/DispatchServletModule.java
@@ -0,0 +1,12 @@
+package com.yardox.server.guice;
+
+import javax.inject.Singleton;
+
+import com.google.inject.servlet.ServletModule;
+
+public class DispatchServletModule extends ServletModule {
+    @Override
+    protected void configureServlets() {
+    }
+}
+

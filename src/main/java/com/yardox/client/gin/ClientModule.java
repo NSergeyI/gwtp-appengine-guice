4555322772cbaa8514694cfdec5ec4ac0e3a120e
SNikitinI <snikitin@akolchin.com>
2019-02-05 13:14:20 +0300

    work app

--- src/main/java/com/yardox/client/gin/ClientModule.java
+++ src/main/java/com/yardox/client/gin/ClientModule.java
@@ -1,5 +1,6 @@
 package com.yardox.client.gin;
 
+import com.gwtplatform.dispatch.rpc.client.gin.RpcDispatchAsyncModule;
 import com.gwtplatform.mvp.shared.proxy.RouteTokenFormatter;
 import com.yardox.client.application.ApplicationModule;
 import com.yardox.client.place.NameTokens;
@@ -9,29 +10,25 @@ import com.gwtplatform.mvp.client.annotations.ErrorPlace;
 import com.gwtplatform.mvp.client.annotations.UnauthorizedPlace;
 import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
 import com.gwtplatform.mvp.client.gin.DefaultModule;
+import com.yardox.client.resources.ResourceLoader;
 
 public class ClientModule extends AbstractPresenterModule {
     @Override
     protected void configure() {
 
         install(new DefaultModule.Builder().tokenFormatter(RouteTokenFormatter.class).build());
+//        install(new DefaultModule.Builder().defaultPlace(NameTokens.HOME).errorPlace(NameTokens.HOME).unauthorizedPlace(NameTokens.HOME).build());
 //        install(new RpcDispatchAsyncModule.Builder().build());
 
         install(new ApplicationModule());
-//        install(new LoginModule());
-
+////        install(new LoginModule());
+//
         // DefaultPlaceManager Places
         bindConstant().annotatedWith(DefaultPlace.class).to(NameTokens.HOME);
         bindConstant().annotatedWith(ErrorPlace.class).to(NameTokens.HOME);
         bindConstant().annotatedWith(UnauthorizedPlace.class).to(NameTokens.HOME);
-//        install(new DefaultModule
-//                .Builder()
-//                .defaultPlace(NameTokens.HOME)
-//                .errorPlace(NameTokens.HOME)
-//                .unauthorizedPlace(NameTokens.HOME)
-//                .build());
 //        install(new ApplicationModule());
-
+//
 //        bind(ResourceLoader.class).asEagerSingleton();
     }
 }
